36ecae0d34362d8b0499667fe8072f33a38878f9
tugot17 <piotr635@gmail.com>
2017-04-19 08:42:45 +0200

    Added Test Methods

---
+++ List 5 Stacks/Tests/Sorts/QuickSortTest.java
@@ -0,0 +1,22 @@
+package Sorts;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by pmazurek on 19.04.2017.
+ */
+public class QuickSortTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void sort() throws Exception {
+
+    }
+
+}

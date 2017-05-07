4234dbebadb96d1f165c4c56b38e9ed2bb9ed937
tugot17 <piotr635@gmail.com>
2017-05-07 15:30:16 +0200

    Quick Sort Implementd
    added quicksort and now quicksort tests are not ignored

--- List 5 Sort/src/Sorts/AdvancedSorts/QuickSort/QuickSort.java
+++ List 5 Sort/src/Sorts/AdvancedSorts/QuickSort/QuickSort.java
@@ -1,6 +1,8 @@
 package Sorts.AdvancedSorts.QuickSort;
 
+import Helpers.RandomGenerator;
 import Helpers.Sort;
+import Messages.SortMessage;
 import Sorts.ISort;
 
 /**
@@ -17,15 +19,60 @@ public class QuickSort extends Sort implements ISort {
     //<editor-fold desc="Public Methods">
     @Override
     public void sort() {
+        this.array = RandomGenerator.createRandomArrayGenerator(amountOfSortedElements);
 
+        sort(array);
     }
 
-
-
     @Override
-    public void sort(int [] array) {
+    public void sort(int[] values) {
+        if (values ==null || values.length==0){
+            return;
+        }
+        this.array = values;
+        amountOfSortedElements = values.length;
+        quicksort(0, amountOfSortedElements - 1);
+
+        SortMessage.showSortInformations("Merge", array.length, amountOfComparasions, stoper.getResult());
 
     }
     //</editor-fold>
 
+    //<editor-fold desc="Private Methods">
+    private void quicksort(int low, int high) {
+        int i = low, j = high;
+        int pivot = array[low + (high-low)/2];
+
+        while (i <= j) {
+
+            while (array[i] < pivot) {
+                i++;
+                amountOfComparasions++;
+            }
+
+            while (array[j] > pivot) {
+                j--;
+                amountOfComparasions++;
+            }
+
+            if (i <= j) {
+                exchange(i, j);
+                i++;
+                j--;
+                amountOfComparasions++;
+            }
+        }
+        if (low < j)
+            quicksort(low, j);
+        if (i < high)
+            quicksort(i, high);
+    }
+
+    private void exchange(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    //</editor-fold>
 }
+

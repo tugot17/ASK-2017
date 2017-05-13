3f727e86fe3f3a69892964c67cb2fe630450b77b
tugot17 <piotr635@gmail.com>
2017-05-13 21:19:24 +0200

    Added Node Class
    Node class is generic

---
+++ List 7 Binary Search Tree/src/BST/Bst.java
@@ -0,0 +1,7 @@
+package BST;
+
+/**
+ * Created by pmazurek on 13.05.2017.
+ */
+public class Bst {
+}
---
+++ List 7 Binary Search Tree/src/BST/Node.java
@@ -0,0 +1,36 @@
+package BST;
+
+/**
+ * Created by pmazurek on 13.05.2017.
+ */
+public class Node <T>{
+
+    //<editor-fold desc="Elements">
+    T value;
+
+    Node left;
+    Node right;
+    //</editor-fold>
+
+    //<editor-fold desc="Constructors">
+    public Node(T value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(T value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Public Methods">
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+    //</editor-fold>
+
+}
